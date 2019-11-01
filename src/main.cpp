d4e06474108983298e000e7dec1e7d9c6c4a121b
semi10 <f.semion89@gmail.com>
2019-11-01 22:10:49 +0200

    ON/OFF Works

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <AsyncJson.h>
 #include <ArduinoJson.h>
 #include <set>
+#include "esp32-hal.h"
 
 #define PWM_FREQ  5000
 #define PWM_RESOLUTION 8
@@ -13,7 +14,21 @@
 #define PWM_CHANNEL_1 1
 #define PWM_CHANNEL_2 2
 
+#define IR_TICK 2.5 //uSec
 
+struct s_irTimes
+{
+    float preamble[2];
+    float logic[2][2];
+};
+
+static s_irTimes samsung { 4500 / IR_TICK, 4500 / IR_TICK, 539 / IR_TICK, 585 / IR_TICK,  539 / IR_TICK, 1709 / IR_TICK };
+static s_irTimes airConditioner { 2900 / IR_TICK, 2900 / IR_TICK, 900 / IR_TICK, 1000 / IR_TICK, 1000 / IR_TICK, 900 / IR_TICK };
+
+rmt_data_t data[256];
+rmt_obj_t* rmt_send = NULL;
+
+void fillData(byte *cmd, rmt_data_t *data, byte size);
 
 struct AirConditioner {
   uint8_t IR_Pin;
@@ -65,7 +80,19 @@ class CaptiveRequestHandler : public AsyncWebHandler {
 
 
 void setup(){
-  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  // IR Setup
+  if ((rmt_send = rmtInit(18, true, RMT_MEM_64)) == NULL)
+  {
+    Serial.println("init sender failed\n");
+  }
+
+  rmtSetCarrier(rmt_send, true, 1, 1050, 1050);
+  float realTick = rmtSetTick(rmt_send, IR_TICK * 1000);
+  printf("real tick set to: %fns\n", realTick);
+
+
   pinMode(AC200.IR_Pin, OUTPUT);    
   pinMode(AC201.IR_Pin, OUTPUT); 
   pinMode(AC202.IR_Pin, OUTPUT); 
@@ -81,10 +108,6 @@ void setup(){
   ledcAttachPin(AC200.IR_Pin, AC200.PWM_Channel);
   ledcAttachPin(AC201.IR_Pin, AC201.PWM_Channel);
   ledcAttachPin(AC202.IR_Pin, AC202.PWM_Channel);
-
-
-
-  Serial.begin(115200);
 
 
   // Initialize SPIFFS
@@ -144,6 +167,10 @@ void handleJSON(JsonObject& jsonObj)
   {
     currentAC->State ^= 1;
 
+    byte cmd[] = {0x9C, 0x1C, 0x00, 0x00};  // On/Off
+    fillData(cmd, data, sizeof(cmd));
+    rmtWrite(rmt_send, data, 106);
+
     if (currentAC->State == HIGH) ledcWrite(currentAC->PWM_Channel, currentAC->Temperature);
     else ledcWrite(currentAC->PWM_Channel, LOW);
   } 
@@ -155,5 +182,66 @@ void handleJSON(JsonObject& jsonObj)
     currentAC->Temperature = tempVal;
     ledcWrite(currentAC->PWM_Channel, currentAC->Temperature);
   }
+  else if (!strcmp(CMD, "Source"))
+  {
+    byte cmd[] = {0xE0, 0xE0, 0x80, 0x7F}; // Source
+    fillData(cmd, data, sizeof(cmd));
+    rmtWrite(rmt_send, data, 34);
+  }
   else Serial.println("???");
+}
+
+void fillData(byte *cmd, rmt_data_t *data, byte size) 
+{
+  int index = 0;
+  int bit = 0;
+
+  for (int repeat = 0; repeat < 3; repeat++)
+  {
+    //Preamble Byte
+    data[index].duration0 = airConditioner.preamble[0];
+    data[index].level0 = 1;
+    data[index].duration1 = airConditioner.preamble[1];
+    data[index].level1 = 0;
+    index++;
+    
+    for (int i = 0; i < size; i++)
+    {
+      uint8_t mask = 0x80;
+      for (int j = 0; j < 8; j++)
+      {
+        bit = bool(cmd[i] & mask);
+        data[index].duration0 = airConditioner.logic[bit][0];
+        data[index].level0 = !bit;
+        data[index].duration1 = airConditioner.logic[bit][1];
+        data[index].level1 = bit;
+        index++;
+        
+        mask >>=1;
+      }
+    }  
+
+    // '1'
+    bit = 1;
+    data[index].duration0 = airConditioner.logic[bit][0];
+    data[index].level0 = !bit;
+    data[index].duration1 = airConditioner.logic[bit][1];
+    data[index].level1 = bit;
+    index++;
+
+    // '0'
+    bit = 0;
+    data[index].duration0 = airConditioner.logic[bit][0];
+    data[index].level0 = !bit;
+    data[index].duration1 = airConditioner.logic[bit][1];
+    data[index].level1 = bit;
+    index++;
+  }
+
+  // Finish 
+  data[index].duration0 = airConditioner.preamble[0];
+  data[index].level0 = 1;
+  data[index].duration1 = airConditioner.logic[0][0];
+  data[index].level1 = 1;
+  index++;
 }
